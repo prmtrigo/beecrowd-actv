45bcc1340cef22afa7db850f015488d248e45cc6
prmtrigo <trigopaulo1@gmail.com>
2024-03-11 19:25:01 -0300

    resolvendo erros

--- 1051.py
+++ 1051.py
@@ -3,7 +3,7 @@ def calcular_imposto_renda(salario):
         return 0
     elif salario <= 3000:
         return (salario - 2000) * 0.08
-    elif salario < 4500:
+    elif salario <= 4500:
         return calcular_imposto_renda(3000) + (salario - 3000) * 0.18
     else:
         return calcular_imposto_renda(4500) + (salario - 4500) * 0.28
